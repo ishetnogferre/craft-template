ed9bd3f5e3e537be5fefd4ed87d0cb176454a7c7
Rias <hello@rias.be>
2018-02-19 23:02:23 +0100

    Add sections & blocks

---
+++ config/expanded-singles.php
@@ -0,0 +1,10 @@
+<?php
+return [
+
+    // Expands the Singles link on the Entries page to list them like Channels and Structures.
+    'expandSingles' => true,
+
+    // Automatically redirects to edit the Single Entry when clicking on the item in the sidebar.
+    'redirectToEntry' => true,
+
+];
