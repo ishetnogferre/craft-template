8162a692b94b24f406dac4efe179dd49320b9363
Rias <hello@rias.be>
2018-03-17 21:22:52 +0100

    Update Laravel Mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,11 +2,12 @@
 /* global path */
 const mix = require('laravel-mix');
 require('laravel-mix-purgecss');
+require('laravel-mix-tailwind');
+require('laravel-mix-banner');
 
 /**
  * Packages used for build
  */
-const webpack = require('webpack');
 const moment = require('moment');
 const gitRevSync = require('git-rev-sync');
 const htmlCritical = require('html-critical-webpack-plugin');
@@ -15,30 +16,10 @@ const copy = require('copy');
 // package vars
 const pkg = require('./package.json');
 
-// Our banner
-const banner = (function () {
-    return [
-        '/**',
-        ' * @project        Marbles Website',
-        ' * @author         Marbles',
-        ' * @build          ' + moment().format('llll') + ' GMT+1',
-        ' * @release        ' + gitRevSync.long() + ' [' + gitRevSync.branch() + ']',
-        ' * @copyright      Copyright (c) ' + moment().format('YYYY') + ', Marbles',
-        ' *',
-        ' */',
-        '',
-    ].join('\n');
-})();
-
 /**
  * Webpack plugins
  */
-let webpackPlugins = [
-    new webpack.BannerPlugin({
-        banner: banner,
-        raw: true,
-    }),
-];
+let webpackPlugins = [];
 
 /**
  * Webpack plugins that only need to be run in production
@@ -77,11 +58,25 @@ if (mix.inProduction()) {
  * Start the Mix function
  */
 mix
+    .banner({
+        banner: (function () {
+            return [
+                '/**',
+                ' * @project        Marbles Website',
+                ' * @author         Marbles',
+                ' * @build          ' + moment().format('llll') + ' GMT+1',
+                ' * @release        ' + gitRevSync.long() + ' [' + gitRevSync.branch() + ']',
+                ' * @copyright      Copyright (c) ' + moment().format('YYYY') + ', Marbles',
+                ' *',
+                ' */',
+                '',
+            ].join('\n');
+        })(),
+        raw: true,
+    })
     .options({
-        processCssUrls: false, // Process/optimize relative stylesheet url()'s. Set to false, if you don't want them touched.
         postCss: [
             require('postcss-object-fit-images'),
-            require('tailwindcss')('./tailwind.js'),
         ],
     })
     .webpackConfig({
@@ -107,7 +102,9 @@ mix
     .js('./src/js/site.js', './web/dist/js/')
     .extract(Object.keys(pkg.dependencies))
     .sass('./src/scss/site.scss', './web/dist/css')
+    .tailwind()
     .purgeCss({
+        enabled: true,
         globs: [
             path.join(__dirname, '/templates/**/*.{html,twig}'),
             path.join(__dirname, '/src/scss/*.scss'),
