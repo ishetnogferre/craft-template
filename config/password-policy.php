59c9ae86734159ab021214a561e987d97e80b2fe
Rias <hello@rias.be>
2018-04-23 09:40:10 +0200

    Add typogrify settings + password policy

---
+++ config/password-policy.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    // Minimum password length
+    "minLength" => 16,
+
+    // Maximum password length
+    "maxLength" => 160,
+
+    // Force users to use different cases
+    "cases" => false,
+
+    // Require at least 1 number
+    "numbers" => false,
+
+    // Require at least one symbol
+    "symbols" => false,
+
+    // Check the Have I been Pwned database
+    "checkPwned" => true,
+
+    // Show a password strength indicator
+    "showStrengthIndicator" => true,
+];
