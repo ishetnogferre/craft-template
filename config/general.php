c829ae8126a2520b09ceb14e71731af1dc4c24d9
Rias <hello@rias.be>
2019-01-20 13:27:40 +0100

    Ch-ch-changes

--- config/general.php
+++ config/general.php
@@ -32,21 +32,15 @@ return [
             'subLeft' => true,
             'subRight' => true,
         ],
-        'enableCsrfProtection' => true,
         'generateTransformsBeforePageLoad' => true,
         'omitScriptNameInUrls' => true,
         'securityKey' => getenv('SECURITY_KEY'),
-        'usePathInfo' => true,
 
         // Aliases parsed in sites’ settings, volumes’ settings, and Local volumes’ settings
         'aliases' => [
             '@basePath' => getenv('BASE_PATH'),
             '@baseUrl' => getenv('BASE_URL'),
         ],
-        // Custom site-specific config settings
-        'custom' => [
-            'craftEnv' => CRAFT_ENVIRONMENT,
-        ]
     ],
 
     // Live (production) environment
@@ -58,12 +52,6 @@ return [
         'devMode' => false,
         'enableTemplateCaching' => true,
         'isSystemLive' => true,
-        // Aliases parsed in sites’ settings, volumes’ settings, and Local volumes’ settings
-        'aliases' => [
-        ],
-        // Custom site-specific config settings
-        'custom' => [
-        ]
     ],
 
     // Staging (pre-production) environment
@@ -76,27 +64,15 @@ return [
         'devMode' => false,
         'enableTemplateCaching' => true,
         'isSystemLive' => false,
-        // Aliases parsed in sites’ settings, volumes’ settings, and Local volumes’ settings
-        'aliases' => [
-        ],
-        // Custom site-specific config settings
-        'custom' => [
-        ]
     ],
 
     // Local (development) environment
-    'local' => [
+    'dev' => [
         // Craft defined config settings
         'allowUpdates' => true,
         'backupOnUpdate' => true,
         'devMode' => true,
         'enableTemplateCaching' => false,
         'isSystemLive' => true,
-        // Aliases parsed in sites’ settings, volumes’ settings, and Local volumes’ settings
-        'aliases' => [
-        ],
-        // Custom site-specific config settings
-        'custom' => [
-        ]
     ],
 ];
