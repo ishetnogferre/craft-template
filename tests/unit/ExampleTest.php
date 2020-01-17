8dba29009ff34eb04daa75e09701353ee9e276ad
kjellknapen <knapenkjell@gmail.com>
2020-01-17 13:47:45 +0100

    implemented tests into craft template

---
+++ tests/unit/ExampleTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace craft\tests;
+
+use Codeception\Test\Unit;
+
+use UnitTester;
+use Craft;
+
+class ExampleTest extends Unit
+{
+    /**
+     * @var UnitTester
+     */
+    protected $tester;
+
+    public function testExample()
+    {
+        Craft::$app->setEdition(Craft::Pro);
+
+        $this->assertSame(
+            Craft::Pro,
+            Craft::$app->getEdition());
+    }
+}
