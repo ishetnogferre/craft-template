0f41325541093a4e8d38ff74d4617002a5bcdc9b
Ferre <ferrelambert@hotmail.com>
2021-03-16 22:55:09 +0100

    feat: add more blocks

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const colors = require('tailwindcss/colors')
 
 module.exports = {
   purge: {
-    enabled: true,
+    enabled: false,
     mode: 'all',
     content: [
       './templates/**/**/*.twig',
